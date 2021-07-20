3aa06b843506f8a52e3c6f19455b763e22104fc3
Shidqi Yusri <shidqifadhlu@gmail.com>
2021-07-20 14:01:40 +0700

    add auth to buatAkunRS and buatAkunPMI

--- routers/adminRouter.js
+++ routers/adminRouter.js
@@ -5,17 +5,13 @@ const {
   login,
   membuatArtikel,
 } = require('../controllers/adminController');
-const auth = require("../middleware/auth");
+const { authenticateToken } = require("../middleware/auth");
 
 const router = express.Router();
 
-router.post("/buat-akun-rs", buatAkunRS);
-router.post("/buat-akun-pmi", buatAkunPMI);
 router.post("/login", login);
-router.post(
-  "/post-artikel",
-  auth.authenticateToken,
-  membuatArtikel
-);
+router.post("/buat-akun-rs", authenticateToken, buatAkunRS);
+router.post("/buat-akun-pmi", authenticateToken, buatAkunPMI);
+router.post("/post-artikel", authenticateToken, membuatArtikel);
 
 module.exports = router;
