601f8f40ea15da3da186931c0aa59656a2bd3312
Shidqi Yusri <shidqifadhlu@gmail.com>
2021-07-20 14:19:39 +0700

    add more function to adminController

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -4,9 +4,9 @@ const jwt = require('jsonwebtoken');
 
 const genToken = (id) => {
   return jwt.sign({ id : id }, process.env.TOKEN_SECRET);
-}
+};
 
-function buatAkunRS(req, res, next) {
+const buatAkunRS = (req, res, next) => {
   try {
     req.body.password = req.body.password
       ? md5(req.body.password)
@@ -14,18 +14,18 @@ function buatAkunRS(req, res, next) {
 
     db.rumahsakit
       .create(req.body)
-      .then(result => {
+      .then((result) => {
         res.rest.created("Akun telah dibuat!");
     })
-      .catch(err => {
+      .catch((err) => {
         res.rest.badRequest(err);
     });
   } catch (error) {
-    next(error)
-  }
-}
+    next(error);
+  };
+};
 
-function buatAkunPMI(req, res, next) {
+const buatAkunPMI = (req, res, next) => {
   try {
     req.body.password = req.body.password
       ? md5(req.body.password)
@@ -33,18 +33,18 @@ function buatAkunPMI(req, res, next) {
 
     db.PMI
       .create(req.body)
-      .then(result => {
+      .then((result) => {
         res.rest.created("Akun telah dibuat!");
     })
-      .catch(err => {
+      .catch((err) => {
         res.rest.badRequest(err);
     });
   } catch (error) {
-    next(error)
-  }
-}
+    next(error);
+  };
+};
 
-function login(req, res, next) {
+const login = (req, res, next) => {
   db.admin
     .findOne({
       where : {
@@ -52,40 +52,100 @@ function login(req, res, next) {
         password: md5(req.body.password)
       }
       })
-    .then(result => {
+    .then((result) => {
       if (result) {
         res.rest.success({
           token: genToken(result.id),
         })
       } else {
         res.rest.unauthorized("Email atau password Anda salah!");
-      }
-      })
-    .catch(error => {
+      };
+    })
+    .catch((error) => {
       next(error);
     });
-}
+};
 
-function membuatArtikel(req, res) {
+const membuatArtikel = (req, res) => {
   const postArtikel = {
     id_admin: req.user.id,
     judul: req.body.judul,
     link: req.body.link,
     thumbnail: req.body.thumbnail,
-  }
+  };
 
   db.artikel
-    .create(postArtikel).then(result => {
+    .create(postArtikel).then((result) => {
       res.rest.created("Artikel berhasil dibuat!");
   })
-    .catch(error => {
+    .catch((error) => {
       res.rest.badRequest(err);
   });
-}
+};
+
+const lihatRequestDarah = (req, res, next) => {
+  db.requestdarah
+    .findAll()
+    .then((result) => {
+      res.rest.success(result);
+    })
+    .catch((error) => {
+      next(error);
+    });
+};
+
+const specificRequestDarah = async (req, res, next) => {
+  try {
+    const dataRequest = await db.requestdarah.findOne({
+      where: { id: req.params.id },
+    });
+
+    if (!dataRequest)
+      return res.rest.badRequest(
+        `Request Darah dengan ID ${req.params.id} tidak ditemukan`
+      );
+
+    res.rest.success({ request: dataRequest });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const lihatEvent = (req, res, next) => {
+  db.eventPMI
+    .findAll()
+    .then((result) => {
+      res.rest.success(result);
+    })
+    .catch((error) => {
+      next(error);
+    });
+};
+
+const specificEvent = async (req, res, next) => {
+  try {
+    const dataEvent = await db.eventPMI.findOne({
+      where: { id: req.params.id },
+    });
+
+    if (!dataEvent)
+      return res.rest.badRequest(
+        `Event dengan ID ${req.params.id} tidak ditemukan`
+      );
+
+    res.rest.success({ event: dataEvent });
+  } catch (error) {
+    next(error);
+  }
+};
 
 module.exports = {
   buatAkunRS,
   buatAkunPMI,
   login,
   membuatArtikel,
+  lihatRequestDarah,
+  specificRequestDarah,
+  lihatEvent,
+  specificEvent,
 }
--- routers/adminRouter.js
+++ routers/adminRouter.js
@@ -4,6 +4,10 @@ const {
   buatAkunPMI,
   login,
   membuatArtikel,
+  lihatRequestDarah,
+  lihatEvent,
+  specificRequestDarah,
+  specificEvent,
 } = require('../controllers/adminController');
 const { authenticateToken } = require("../middleware/auth");
 
@@ -13,5 +17,9 @@ router.post("/login", login);
 router.post("/buat-akun-rs", authenticateToken, buatAkunRS);
 router.post("/buat-akun-pmi", authenticateToken, buatAkunPMI);
 router.post("/post-artikel", authenticateToken, membuatArtikel);
+router.post("/request", authenticateToken, lihatRequestDarah);
+router.post("/event", authenticateToken, lihatEvent);
+router.post("/request/:id", authenticateToken, specificRequestDarah);
+router.post("/eventt/:id", authenticateToken, specificEvent);
 
 module.exports = router;
