76519aa5b153b25a2949961dd20d911c9a356b7c
vitomichael <hanvitomichaellee@student.telkomuniversity.ac.id>
2021-08-03 14:30:54 +0700

    add more attribute

--- models/donorDarahPMI.js
+++ models/donorDarahPMI.js
@@ -36,6 +36,11 @@ module.exports = (sequelize, DataTypes) => {
         allowNull: false,
         defaultValue: false,
       },
+      selesai: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      },
     },
     {
       sequelize,
